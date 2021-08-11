0b0c6280bdaaa5c0945a5afcb49654da82b2da80
CaioVinicius7 <caio1525pereira@gmail.com>
2021-08-11 16:39:01 -0300

    API completa

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,19 +4,77 @@ module.exports = (app) => {
    
    // Rotas
    app.get("/", (req, res) => {
-      res.send("Servidor rodando, tudo ok!");
+
+      const rotas = [
+         {
+            "função": "Listar um único atendimento",
+            "endpoint": "/atendimento/id",
+            "método http": "GET"
+         },
+         {
+            "função": "Listar todos os atendimentos",
+            "endpoint": "/atendimentos",
+            "método http": "GET"
+         },
+         {
+            "função": "Criar um atendimentos",
+            "endpoint": "/atendimentos/id",
+            "método http": "POST"
+         },
+         {
+            "função": "Editar um atendimento",
+            "endpoint": "/atendimentos/id",
+            "método http": "PATCH"
+         },
+         {
+            "função": "Excluir um atendimento",
+            "endpoint": "/atendimentos/id",
+            "método http": "DELETE"
+         }
+      ];
+
+      res.json(rotas); 
+      
    });
    
+   // Lista todos os atendimentos
    app.get("/atendimentos", (req, res) => {
-      res.send("Você está na rota de atendimentos!");
+      Atendimento.lista(res);
    });
 
+   // Lista um atendimento específico
+   app.get("/atendimento/:id", (req, res) => {
+
+      // Converte o id de string para number
+      const id = parseInt(req.params.id);
+      
+      Atendimento.buscaPorId(id, res);
+      
+   });
+
+   // Adiciona novos atendimentos
    app.post("/atendimentos", (req, res) => {
       const atendimento = req.body;
 
-      // Chama o model de atendimento executando o método de adicionar atendimento
-      Atendimento.adiciona(atendimento);
-      res.send("Você está na rota de atendimentos e está realizando um post!");
+      Atendimento.adiciona(atendimento, res);
+   });
+
+   // Edita um atendimento
+   app.patch("/atendimentos/:id", (req, res) => {
+
+      // Converte o id de string para number e pega os dados que vão ser alterados
+      const id = parseInt(req.params.id);
+      const valores = req.body;
+
+   
+      Atendimento.altera(id, valores, res);   
+   });
+
+   app.delete("/atendimentos/:id", (req, res) => {
+      const id = parseInt(req.params.id);
+
+      Atendimento.deleta(id, res);
+
    });
 
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -4,25 +4,126 @@ const conexao = require("../infraestrutura/conexao");
 class Atendimento{
 
    // Método para adicionar novo atendimento
-   adiciona(atendimento){
+   adiciona(atendimento, res){
       
       // Pega a data de criação, formata a data do atendimento e cria um objeto novo incluindo essas datas
       const dataCriacao = moment().format("YYYY-MM-DD HH:MM:SS");
       const data = moment(atendimento.data, "DD/MM/YYYY").format("YYYY-MM-DD HH:mm:ss");
       const atendimentoDatado = { ...atendimento, dataCriacao, data }; 
 
-      const sql = "INSERT INTO atendimentos SET ?";
+      // Valida se a data do serviço é a mesma ou é depois da data de criaç]ao do atendimento 
+      const dataValida = moment(data).isSameOrAfter(dataCriacao);
 
-      conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+      // Valida se o nome do cliente é maior ou igual a 5 
+      const clientevalido = atendimento.cliente.length >= 5;
+
+      // Objeto para verificar as validações
+      const validacoes = [
+         {
+            nome: "data",
+            valido: dataValida,
+            mensagem: "A data deve ser maior ou igual a data atual"
+         },
+         {
+            nome: "cliente",
+            valido: clientevalido,
+            mensagem: "O nome do cliente deve ter pelo menos 5 caracteres"
+         }
+      ]; 
+
+      // Verifica se existiram erros na validação
+      const erros = validacoes.filter((campo) => !campo.valido);
+      const existemErros = erros.length;
+
+      if(existemErros){
+         res.status(400).json(erros);
+      }else{
+
+         const sql = "INSERT INTO atendimentos SET ?";
+   
+         conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+   
+            if(erro){
+               res.status(400).json(erro);
+            }else{
+               res.status(201).json({atendimento});
+            }
+   
+         });
+
+      }
+      
+   }
+
+   // Método para listar todos os atendimentos
+   lista(res){
+      const sql = "SELECT * FROM atendimentos";
+
+      conexao.query(sql ,(erro, resultados) => {
+         if(erro){
+            res.status(400).json(erro);
+         }else{
+            res.status(201).json(resultados);
+         }
+      });
+   }
+
+   // Método para buscar um atendiment específico por id
+   buscaPorId(id, res){
+
+      const sql = `SELECT * FROM atendimentos WHERE id= ${id}`;
+
+      conexao.query(sql, (erro, resultado) => {
+
+         // Pega somente o objeto ao invés de um array
+         const atendimento = resultado[0];
 
          if(erro){
-            console.log(`Ocorreu um erro ao inserir os dados na tabela: ${erro}`);
+            res.status(400).json(erro);
          }else{
-            console.log(resultados);
+            res.status(200).json(atendimento);
          }
 
       });
-      
+
+   }
+
+   // Edita um atendimento
+   altera(id, valores, res){
+
+      // Verifica se o campo data existe, caso exista formata a data
+      if(valores.data){
+         valores.data = moment(valores.data, "DD/MM/YYYY").format("YYYY-MM-DD HH:mm:ss");
+      }
+
+      const sql = "UPDATE atendimentos Set ? WHERE id = ?";
+
+      conexao.query(sql, [valores, id], (erro, resultado) => {
+
+         if(erro){
+            res.status(400).json(erro);
+         }else{
+            res.status(200).json({ ...valores, id });
+         }
+
+      });
+
+   }
+
+   deleta(id, res){
+
+      const sql = "DELETE FROM atendimentos WHERE id = ?";
+
+      conexao.query(sql, id, (erro, resultado)=> {
+
+         if(erro){
+            res.status(400).json(erro);
+         }else{
+            res.status(200).json({id});
+         }
+
+      });
+
    }
 
 }
